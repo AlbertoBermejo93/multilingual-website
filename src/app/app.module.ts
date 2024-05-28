40f60617c2d649e92a80a598f16d5658c30108c7
Alberto Bermejo <albertodeveloppeurweb@gmail.com>
2024-05-28 22:41:50 +0200

    created all the layout components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,18 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { HeroComponent } from './layout/hero/hero.component';
+import { PracticeAreaComponent } from './layout/practice-area/practice-area.component';
+import { ServicesComponent } from './layout/services/services.component';
+import { AboutComponent } from './layout/about/about.component';
+import { ContactComponent } from './layout/contact/contact.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
 }
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, FooterComponent, HeroComponent, PracticeAreaComponent, ServicesComponent, AboutComponent, ContactComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
---
+++ src/app/layout/practice-area/practice-area.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-practice-area',
+  templateUrl: './practice-area.component.html',
+  styleUrl: './practice-area.component.scss'
+})
+export class PracticeAreaComponent {
+
+}
