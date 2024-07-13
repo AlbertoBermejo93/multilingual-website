967ca5ea4aa2cf2049d6908b1f542d5df931bad9
Alberto Bermejo <albertodeveloppeurweb@gmail.com>
2024-07-13 14:57:47 +0200

    added about component and some visual fixes

--- src/app/layout/contact/contact.component.ts
+++ src/app/layout/contact/contact.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
-
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    // emailjs
+    //   .sendForm(
+    //     'YOUR_SERVICE_ID',
+    //     'YOUR_TEMPLATE_ID',
+    //     e.target as HTMLFormElement,
+    //     'YOUR_PUBLIC_KEY'
+    //   )
+    //   .then(
+    //     (result: EmailJSResponseStatus) => {
+    //       console.log(result.text);
+    //     },
+    //     (error) => {
+    //       console.log(error.text);
+    //     }
+    //   );
+  }
 }
